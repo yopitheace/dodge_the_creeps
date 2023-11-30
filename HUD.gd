4b638c2412f111c0bc008fbc9a2e71e59fdf42c2
EvanG <148266009+yopitheace@users.noreply.github.com>
2023-11-30 13:18:51 -0500

    Add files via upload

---
+++ HUD.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+
+#Notifies the main scene that the start button has been pressed.
+signal start_game
+
+func show_message(text):
+	$Message.text = (text)
+	$Message.show()
+	$MessageTimer.start()
+	
+func show_game_over():
+	show_message("Game Over")
+	#Wait for the message timer to end
+	await $MessageTimer.timeout
+	
+	$Message.text = "Dodge the Creeps!"
+	$Message.show()
+	#Make a one-shot timer and wait for it to finish
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+func _on_message_timer_timeout():
+	$Message.hide()
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
---
+++ README.md
@@ -0,0 +1,5 @@
+# Dodge the Creeps
+
+This game follows "Your first 2D game" tutorial:
+
+https://docs.godotengine.org/en/latest/getting_started/first_2d_game/index.html
